062e1be9896abf446d1df7eb5d52add455edda67
Sarthak <stanwani7@gmail.com>
2021-11-28 14:15:44 -0800

    Able to parse the start and stop simulation times\n2. can calculate the total time taken to send a packet over the wireless link from the simulation output

--- software/integrator.sh
+++ software/integrator.sh
@@ -7,7 +7,22 @@ NS3_PATH=/home/sarthak/Documents/for_embedded_systems_project/standalone_ns3/rep
 
 cd $NS3_PATH
 export 'NS_LOG=UdpEchoClientApplication=level_all|prefix_func|prefix_time:UdpEchoServerApplication=level_all|prefix_func|prefix_time'
-echo $($NS3_PATH/waf --run "scratch/WiFi_simulation --nCsma=1 --nWifi=2 --dataLen=2044" | grep "At")
+$NS3_PATH/waf --run "scratch/WiFi_simulation --nCsma=1 --nWifi=2 --dataLen=2044" > log.out 2>&1
+s=$(grep -A1 "At time 4s" ./log.out | awk '{print $1;}')
+#s=${s//[!0-9,.]/}
+echo "start output"
+
+echo "The two times are: $s"
+startTime=$(echo $s | awk '{print $1;}')
+startTime=${startTime//[!0-9,.]/}
+echo "Start time is: $startTime"
+stopTime=$(echo $s|awk '{print $2;}')
+stopTime=${stopTime//[!0-9,.]/}
+echo "Stop time is: $stopTime"
+totalTime=$(bc -l <<<"${stopTime}-${startTime}")
+echo "Total time is: $totalTime"
+totalTime=$(bc -l <<<"${totalTime}*1000")
+echo "Total time in ms is: $totalTime"
 cd 
 
 #to take arguments in a bash file
