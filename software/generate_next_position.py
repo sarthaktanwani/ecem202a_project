f839e4155b329041f1fd59b95ad1ff0fc31d58e1
Pragya Sharma <pragyasharma@ucla.edu>
2021-12-05 15:25:43 -0800

    Update to integrator script

--- software/generate_next_position.py
+++ software/generate_next_position.py
@@ -9,27 +9,27 @@ f = open ("next_position.txt", 'w')
 # Z value remains constant throughout simulation
 
 f.write('Start \n')
-for x in np.arange(-11.56,6.16,0.1):
+for x in np.arange(-11.56,6.16,0.5):
     f.write(str(x))
     f.write('\n')
 
 f.write('First Checkpoint \n')
-for y in np.arange(-7.12,6.88,0.1):
+for y in np.arange(-7.12,6.88,0.5):
     f.write(str(y))
     f.write('\n')
 
 f.write('Second Checkpoint \n')
-for x in np.arange(6.16,-16.24,-0.1):
+for x in np.arange(6.16,-16.24,-0.55):
     f.write(str(x))
     f.write('\n')
 
 f.write('Third Checkpoint \n')
-for y in np.arange(6.88,1.72,-0.1):
+for y in np.arange(6.88,1.72,-0.5):
     f.write(str(y))
     f.write('\n')
 
 f.write('Fourth Checkpoint \n')
-for x in np.arange(-16.24,-5.21,0.1):
+for x in np.arange(-16.24,-5.21,0.5):
     f.write(str(x))
     f.write('\n')
 
--- software/integrator.sh
+++ software/integrator.sh
@@ -2,110 +2,217 @@
 #add thing to get overall delay
 ultimateStartTime=`date +%s.%3N`
 source /opt/ros/melodic/setup.bash
-start=`date +%s.%3N`
-#sleep 1
-STRING=$(rostopic echo /gazebo/model_states -n1 | grep -A3 -m2 "position" | tail -n3)
-#STRING="x=45,y=45,z=0"
-len=`expr length "$STRING"`
-end=`date +%s.%3N`
-runtime=$(bc -l <<<"${end}-${start}")
-echo "The length of $STRING is $len"
-echo "Start time is $start"
-echo "End time is $end"
-echo "Status Latency is $runtime"
 
 NS3_PATH=~/Software/ns-allinone-3.30.1/ns-3.30.1
 
+save_pwd=$(pwd);
 cd $NS3_PATH
 export 'NS_LOG=UdpEchoClientApplication=level_all|prefix_func|prefix_time:UdpEchoServerApplication=level_all|prefix_func|prefix_time'
-$NS3_PATH/waf --run "scratch/WiFi_simulation --nCsma=1 --nWifi=2 --dataLen=2044" > log.out 2>&1
-s=$(grep -A1 "At time 4s" ./log.out | awk '{print $1;}')
-#s=${s//[!0-9,.]/}
-echo "start output"
-
-echo "The two times are: $s"
-startTime=$(echo $s | awk '{print $1;}')
-startTime=${startTime//[!0-9,.]/}
-echo "Start time is: $startTime"
-stopTime=$(echo $s|awk '{print $2;}')
-stopTime=${stopTime//[!0-9,.]/}
-echo "Stop time is: $stopTime"
-totalTime=$(bc -l <<<"${stopTime}-${startTime}")
-echo "Total time is: $totalTime"
-totalTime=$(bc -l <<<"${totalTime}*1000")
-echo "Total time in ms is: $totalTime"
-cd 
+cd $save_pwd
 
 echo $pwd
-#input="/home/pragyasharma/ECE202/ecem202a_project/software/demo.txt"
 
-#while IFS= read -r line
-#do
-#  echo "$line"
-#done < "$input"
+input="next_position.txt"
+line_x1=$(grep -n "Start" next_position.txt)
+line_x1=${line_x1//[!0-9]/}
+line_y1=$(grep -n "First Checkpoint" next_position.txt)
+line_y1=${line_y1//[!0-9]/}
+line_x2=$(grep -n "Third Checkpoint" next_position.txt)
+line_x2=${line_x2//[!0-9]/}
+line_y2=$(grep -n "Fourth Checkpoint" next_position.txt)
+line_y2=${line_y2//[!0-9]/}
+last_line=$(grep -n "end" next_position.txt)
+last_line=${last_line//[!0-9]/}
 
-sed -n '5p' < "/home/pragyasharma/ECE202/ecem202a_project/software/demo.txt"
+# echo "linex1 is $line_x1"
+# echo "linex1 is $line_x1"
+# echo "linex1 is $line_x1"
+# echo "linex1 is $line_x1"
 
-cmd=$("pwd")
-echo $cmd
 
-#rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState \"model_name: 'diff_drive' '{linear'
+# x=1
+# while [ $x -lt 5 ]
+# do
+#   echo "Welcome $x times"
+#   x=$(( $x + 1 ))
+# done
 
-template1=$("rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState \"model_name: 'diff_drive'\n")
-pose=$("pose:\n")
-position=$("  position:\n")
-px1=$("    x: 10\n")
-py1=$("    y: 10\n")
-pz1=$("    z: 10.0\n")
-orientation=$("  orientation:\n")
-px2=$("    x: 0.0\n")
-py2=$("    y: 0.0\n")
-pz2=$("    z: 0.0\n")
-pw1=$("    w: 0.0\n")
-twist=$("twist:\n")
-linear=$("  linear:\n")
-px3=$("    x: 0.0\n")
-py3=$("    y: 0.0\n")
-pz3=$("    z: 0.0\n")
-angular=$("  angular:\n")
-px4=$("    x: 0.0\n")
-py4=$("    y: 0.0\n")
-pz4=$("    z: 0.0\n")
-pref=$("reference_frame: ''\"\n")
-pdq=$("\n)
-echo $template
-echo $pose
-echo $position
-echo $px1
-echo $py1
-echo $pz1
-echo $orientation
-echo $px2
-echo $py2
-echo $pz2
-echo $pw1
-echo $twist
-echo $linear
-echo $px3
-echo $py3
-echo $pz3
-echo $angular
-echo $px4
-echo $py4
-echo $pz4
-echo $pref
-echo $pdq
+line_number=2
+timestamp=`date +%s`
+printf "GazeboRead,NS3Link1,ReadFile,NS3Link2,GazeboPublish,SumLatency,StepRuntime\n" >> log_$timestamp.csv
+# printf "$host\t$(date)\tTime Taken to checkout\t$Time_checkout\n" >> log.csv
+# printf "$host\t$(date)\tTime Taken to add $loopmax 10MB svn files\t$Time_add\n" >> log.csv
+# printf "$host\t$(date)\tTime Taken to commit $loopmax 10MB svn files\t$Time_commit\n" >> log.csv
+
+while [ $line_number -lt $last_line ]
+do
+
+  start_step=`date +%s.%3N`
+
+  # read current_pos
+  gazebo_read_start=`date +%s.%3N`
+  # start_step=gazebo_read_start;
+  STRING=$(rostopic echo /gazebo/model_states -n1 | grep -A3 -m2 "position" | tail -n3)
+  #STRING="x=45,y=45,z=0"
+  len=`expr length "$STRING"`
+  gazebo_read_end=`date +%s.%3N`
+  gazebo_read_runtime=$(bc -l <<<"${gazebo_read_end}-${gazebo_read_start}")
+  gazebo_read_runtime=$(bc -l <<<"${gazebo_read_runtime}*1000")
+  # echo "The length of $STRING is $len"
+  # echo "Start time is $start"
+  # echo "End time is $end"
+  echo "Gazebo Read Latency in ms is $gazebo_read_runtime"
+
+  ns3_app_start=`date +%s.%3N`
+
+  #simulate one ns3 link
+  save_pwd=$(pwd);
+  cd $NS3_PATH
+  # export 'NS_LOG=UdpEchoClientApplication=level_all|prefix_func|prefix_time:UdpEchoServerApplication=level_all|prefix_func|prefix_time'
+  $NS3_PATH/waf --run "scratch/WiFi_simulation --nCsma=1 --nWifi=2 --dataLen=2044" > log.out 2>&1
+  s=$(grep -A5 "At time 4s" ./log.out | awk '{print $1;}')
+  #s=${s//[!0-9,.]/}
+  #echo "s is $s"
+  #echo "start output"
+
+  #echo "The six times are: $s"
+  ns3_start1=$(echo $s | awk '{print $1;}')
+  ns3_start1=${ns3_start1//[!0-9,.]/}
+  #echo "Start time is: $ns3_start1"
+  ns3_stop1=$(echo $s|awk '{print $2;}')
+  ns3_stop1=${ns3_stop1//[!0-9,.]/}
+  #echo "Stop time is: $ns3_stop1"
+  ns3_runtime1=$(bc -l <<<"${ns3_stop1}-${ns3_start1}")
+  #echo "Total time is: $ns3_runtime1"
+  ns3_runtime1=$(bc -l <<<"${ns3_runtime1}*1000")
+  echo "Link Latency1 in ms is: $ns3_runtime1"
+
+  ns3_start2=$(echo $s | awk '{print $5;}')
+  ns3_start2=${ns3_start2//[!0-9,.]/}
+  #echo "Start time is: $ns3_start2"
+  ns3_stop2=$(echo $s|awk '{print $6;}')
+  ns3_stop2=${ns3_stop2//[!0-9,.]/}
+  #echo "Stop time is: $ns3_stop2"
+  ns3_runtime2=$(bc -l <<<"${ns3_stop2}-${ns3_start2}")
+  #echo "Total time is: $ns3_runtime2"
+  ns3_runtime2=$(bc -l <<<"${ns3_runtime2}*1000")
+  echo "Link Latency2 in ms is: $ns3_runtime2"
+
+  cd $save_pwd
+
+  ns3_app_stop=`date +%s.%3N`
+
+  ns3_app_runtime=$(bc -l <<<"${ns3_app_stop}-${ns3_app_start}")
+  ns3_app_runtime=$(bc -l <<<"${ns3_app_runtime}*1000")
+  echo "NS3 app Latency in ms is $ns3_app_runtime"
+
+  comp_start=`date +%s.%3N`
+  new_position=$(sed "${line_number}q;d" next_position.txt)
+  comp_end=`date +%s.%3N`
+  comp_time=$(bc -l <<<"${comp_end}-${comp_start}")
+  echo "Comp Latency in ms is $comp_time"
+
+
+  gazebo_pub_start=`date +%s.%3N`
+  if [[ $line_number -lt $line_y1 ]]
+  then
+    sed -i "4s/^.*$/    x: $new_position/" test.yaml
+    rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState -f test.yaml
+
+  elif [[ $line_number -lt $line_x2 ]]
+  then
+    sed -i "4s/^.*$/    y: $new_position/" test.yaml
+    rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState -f test.yaml
+
+  elif [[ $line_number -lt $line_y2 ]]
+  then
+    sed -i "4s/^.*$/    x: $new_position/" test.yaml
+    rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState -f test.yaml
+
+  elif [[ $line_number -lt $last_line ]]
+  then
+    sed -i "4s/^.*$/    y: $new_position/" test.yaml
+    rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState -f test.yaml
+  fi
+
+  gazebo_pub_end=`date +%s.%3N`
+  gazebo_pub_runtime=$(bc -l <<<"${gazebo_pub_end}-${gazebo_pub_start}")
+  gazebo_pub_runtime=$(bc -l <<<"${gazebo_pub_runtime}*1000")
+  echo "Gazebo Write Latency in ms is: $gazebo_pub_runtime"
+
+  line_number=$(( $line_number + 1 ))
+
+  sum_runtime=$(bc -l <<<"${gazebo_read_runtime}+${ns3_runtime1}+${comp_time}+${ns3_runtime2}+${gazebo_pub_runtime}")
+  echo "Sum runtime is $sum_runtime"
+
+  stop_step=`date +%s.%3N`
+  step_runtime=$(bc -l <<<"${stop_step}-${start_step}")
+  step_runtime=$(bc -l <<<"${step_runtime}*1000")
+  echo "Total step runtime is $step_runtime"
+
+
+printf "${gazebo_read_runtime},${ns3_runtime1},${comp_time},${ns3_runtime2},${gazebo_pub_runtime},$sum_runtime,$step_runtime\n" >> log_$timestamp.csv
+
+done
+
+# values="none"
+# while IFS= read -r line
+# do
+
+  
+
+#   if values="x"
+#   then
+#     x_val=$line
+#     sed -i "4s/^.*$/    x: $x_val/" test.yaml
+#     rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState -f test.yaml
+#   fi
+#   if $line="Start"
+#   then
+#     values="x"
+#   fi
+#   elif $line="First Checkpoint"
+#   then
+#     values="y"
+#   fi
+#   elif $line="Second Checkpoint "
+#   then
+#     values="x"
+#   fi
+
+# done < "$input"
+
+
+
+# x_val=-14.54
+# y_val=0.0
+# z_val=0.0
+
+# sed -i "4s/^.*$/    x: $x_val/" test.yaml
+# sed -i "5s/^.*$/    y: $y_val/" test.yaml
+# sed -i "6s/^.*$/    z: $z_val/" test.yaml
+
+# rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState -f test.yaml
+
+# x_val=$(sed -n '4p' < "test.yaml")
+# x_val=${x_val//[!0-9,.,-]/}
+
+# y_val=$(sed -n '5p' < "test.yaml")
+# y_val=${y_val//[!0-9,.,-]/}
+
+# z_val=$(sed -n '6p' < "test.yaml")
+# z_val=${z_val//[!0-9,.,-]/}
 
 #to take arguments in a bash file
-echo " I firmly believe that $1 is the best $2 for the office of $3"
+# echo " I firmly believe that $1 is the best $2 for the office of $3"
 
 
-echo $(whoami)
+# echo $(whoami)
 
-echo "What is your name?"
-read name
-if [ $name ]; then
-	echo "$name sounds alright to me"
-else
-	echo "GIVE ME YOUR NAMEE!!!"
-fi
+# echo "What is your name?"
+# read name
+# if [ $name ]; then
+# 	echo "$name sounds alright to me"
+# else
+# 	echo "GIVE ME YOUR NAMEE!!!"
+# fi
