817989754b69e787488ce23d72d4ca932301cf89
Pragya Sharma <pragyasharma@ucla.edu>
2021-12-05 11:01:42 -0800

    Created generate_new_positions.py file and location file

---
+++ software/generate_next_position.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+f = open ("next_position.txt", 'w')
+
+# Start Values 
+# x = -11.56
+# y = -7.12
+# z = 0.128750
+# Z value remains constant throughout simulation
+
+f.write('Start \n')
+for x in np.arange(-11.56,6.16,0.1):
+    f.write(str(x))
+    f.write('\n')
+
+f.write('First Checkpoint \n')
+for y in np.arange(-7.12,6.88,0.1):
+    f.write(str(y))
+    f.write('\n')
+
+f.write('Second Checkpoint \n')
+for x in np.arange(6.16,-16.24,-0.1):
+    f.write(str(x))
+    f.write('\n')
+
+f.write('Third Checkpoint \n')
+for y in np.arange(6.88,1.72,-0.1):
+    f.write(str(y))
+    f.write('\n')
+
+f.write('Fourth Checkpoint \n')
+for x in np.arange(-16.24,-5.21,0.1):
+    f.write(str(x))
+    f.write('\n')
+
+f.close()
