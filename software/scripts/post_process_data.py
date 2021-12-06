94f88ddae72a5755d3123688ca159e29b2e563d0
Pragya Sharma <pragya.sharma@west.cmu.edu>
2021-12-05 21:51:28 -0800

    Reorganized directory structure

---
+++ software/scripts/post_process_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+scenario_1 = "Scenario_1"
+scenario_2 = "Scenario_2"
+directory_list = [scenario_1, scenario_2]
+
+for directory in directory_list:
+    if "1" in directory:
+        avg_data = pd.DataFrame(columns = ['GazeboRead', 'NS3Link1', 'ReadFile', 'NS3Link2', 'GazeboPublish', 'SumLatency'])
+        for file in os.scandir(directory):
+            data = pd.read_csv(file, index_col=False)
+            avg_data = avg_data.append({'GazeboRead':data['GazeboRead'].mean(), 'NS3Link1':data['NS3Link1'].mean(), 'ReadFile':data['ReadFile'].mean(), 'NS3Link2':data['NS3Link2'].mean(), 'GazeboPublish':data['GazeboPublish'].mean(), 'SumLatency':data['SumLatency'].mean()}, ignore_index=True)
+    else:
+        avg_data = pd.DataFrame(columns = ['GazeboRead', 'ReadFile', 'GazeboPublish', 'SumLatency'])
+        for file in os.scandir(directory):
+            data = pd.read_csv(file, index_col=False)
+            avg_data = avg_data.append({'GazeboRead':data['GazeboRead'].mean(), 'ReadFile':data['ReadFile'].mean(), 'GazeboPublish':data['GazeboPublish'].mean(), 'SumLatency':data['SumLatency'].mean()}, ignore_index=True)
+    avg_data.to_csv('{0}/processed.csv'.format(directory), index=False)
