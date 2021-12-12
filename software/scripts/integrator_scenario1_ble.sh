ee53aadb852cee58fb97d22aaa572ab65034c470
Sarthak <stanwani7@gmail.com>
2021-12-12 15:37:02 -0800

    Edits to report, code cleanup for integrator_scenario1_ble.sh file

--- software/scripts/integrator_scenario1_ble.sh
+++ software/scripts/integrator_scenario1_ble.sh
@@ -1,17 +1,13 @@
 #! /bin/bash
-#add thing to get overall delay
-ultimateStartTime=`date +%s.%3N`
+
+#setup ROS
 source /opt/ros/melodic/setup.bash
 
+#setup NS3 Path to run the simulation in
 NS3_PATH=../ns3_simulation/ns-allinone-3.30.1/ns-3.30.1
 
-# save_pwd=$(pwd);
-# cd $NS3_PATH
-# export 'NS_LOG=UdpEchoClientApplication=level_all|prefix_func|prefix_time:UdpEchoServerApplication=level_all|prefix_func|prefix_time'
-# cd $save_pwd
-
-#echo $pwd
-
+# parsing important line numbers from the next position txt file...
+# to be used in the below loop to send next position values to gazebo
 input="../next_position.txt"
 line_x1=$(grep -n "Start" $input)
 line_x1=${line_x1//[!0-9]/}
@@ -30,27 +26,10 @@ sed -i "4s/^.*$/    x: $new_position/" test.yaml
 new_position=$(sed "$(($line_y1+1))q;d" $input)
 sed -i "5s/^.*$/    y: $new_position/" test.yaml
 
-#echo "linex1 is $line_x1"
-#echo "liney1 is $line_y1"
-#echo "linex2 is $line_x2"
-#echo "liney2 is $line_y2"
-#echo "linex3 is $line_x3"
-
-
-# x=1
-# while [ $x -lt 5 ]
-# do
-#   echo "Welcome $x times"
-#   x=$(( $x + 1 ))
-# done
-
 line_number=2
 timestamp=`date +%s`
 printf "GazeboRead,NS3Link1,ReadFile,NS3Link2,GazeboPublish,E2E_Latency,StepRuntime\n" >> ../../data/logs/ble/scenario1/log_scenario1_$timestamp.csv
-# printf "$host\t$(date)\tTime Taken to checkout\t$Time_checkout\n" >> log.csv
-# printf "$host\t$(date)\tTime Taken to add $loopmax 10MB svn files\t$Time_add\n" >> log.csv
-# printf "$host\t$(date)\tTime Taken to commit $loopmax 10MB svn files\t$Time_commit\n" >> log.csv
-#line_number=33
+
 while [ $line_number -lt $last_line ]
 do
 
@@ -139,26 +118,6 @@ do
 
   gazebo_pub_start=`date +%s.%3N`
 
-  # if [[ $line_number -eq $line_y1 ]]
-  # then
-  #   sed -i "8s/^.*$/    x: 1.57/" test.yaml
-  #   rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState -f test.yaml
-
-  # elif [[ $line_number -eq $line_x2 ]]
-  # then
-  #   sed -i "8s/^.*$/    x: 3.1415/" test.yaml
-  #   rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState -f test.yaml
-
-  # elif [[ $line_number -eq $line_y2 ]]
-  # then
-  #   sed -i "8s/^.*$/    x: -1.57/" test.yaml
-  #   rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState -f test.yaml
-
-  # if [[ $line_number -eq $line_x3 ]]
-  # then
-  #   sed -i "8s/^.*$/    x: 0.0/" test.yaml
-  #   rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState -f test.yaml
-
   if [[ $line_number -eq $line_y1 || $line_number -eq $line_x2 ||  $line_number -eq $liney2 || $line_number -eq $line_x3 || $line_number -eq $last_line ]]
   then
     line_number=$(( $line_number + 1 ))
@@ -215,32 +174,3 @@ do
 printf "${gazebo_read_runtime},${ns3_runtime1},${comp_time},${ns3_runtime2},${gazebo_pub_runtime},$sum_runtime,$step_runtime\n" >> ../../data/logs/ble/scenario1/log_scenario1_$timestamp.csv
 
 done
-
-# sed -i "4s/^.*$/    x: $x_val/" test.yaml
-# sed -i "5s/^.*$/    y: $y_val/" test.yaml
-# sed -i "6s/^.*$/    z: $z_val/" test.yaml
-
-# rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState -f test.yaml
-
-# x_val=$(sed -n '4p' < "test.yaml")
-# x_val=${x_val//[!0-9,.,-]/}
-
-# y_val=$(sed -n '5p' < "test.yaml")
-# y_val=${y_val//[!0-9,.,-]/}
-
-# z_val=$(sed -n '6p' < "test.yaml")
-# z_val=${z_val//[!0-9,.,-]/}
-
-#to take arguments in a bash file
-# echo " I firmly believe that $1 is the best $2 for the office of $3"
-
-
-# echo $(whoami)
-
-# echo "What is your name?"
-# read name
-# if [ $name ]; then
-# 	echo "$name sounds alright to me"
-# else
-# 	echo "GIVE ME YOUR NAMEE!!!"
-# fi
